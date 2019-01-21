91a088c629335e3de9b6d875209d8832c57a8e9f
Serigne Amsatou SEYE <serigne-amsatou.seye@etu.u-bordeaux.fr>
2019-01-21 03:06:33 +0100

    Handler for 500 - Intrnal Server Error in a different way

--- src/routes/person.js
+++ src/routes/person.js
@@ -17,4 +17,9 @@ router.get('/person/:name', (req, res) => {
     res.send(`You have requested the person ${req.params.name}`)
 })
 
+router.get('/error', (req, res) => {
+    throw new Error('This is a forced error !')
+})
+
+
 module.exports = router
