b188cc1386ee3746980495d7d6f83a951b55faac
Serigne Amsatou SEYE <serigne-amsatou.seye@etu.u-bordeaux.fr>
2019-01-21 01:38:51 +0100

    Using Query and params properties on he reques object

--- src/routes/person.js
+++ src/routes/person.js
@@ -1,8 +1,20 @@
 let express = require('express')
 let router = express.Router()
 
+// QueryString => query property on the request object
+// localhost:3000/person?name=Hamza&age10
 router.get('/person', (req, res) => {
-    res.send('You have requested a person')
+    if (req.query.name) {
+        res.send(`You have requested the person ${req.query.name}`)        
+    } else {
+        res.send('You have requested a person')
+    }
+})
+
+// Params property on the request object
+// localhost:3000/person/Hamza
+router.get('/person/:name', (req, res) => {
+    res.send(`You have requested the person ${req.params.name}`)
 })
 
 module.exports = router
